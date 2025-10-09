9de77a1c1b72fe578e8878c04165f6cc41f60fb5
bensouici akram <bensouiciakram@gmail.com>
2025-10-09 13:31:04 +0100

    adding correct name into channel model

--- alert/models.py
+++ alert/models.py
@@ -3,6 +3,9 @@ from products.models import Product
 
 class Channel(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
     
 
 class Alert(models.Model):
