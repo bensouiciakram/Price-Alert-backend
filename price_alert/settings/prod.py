283f6e01f08843263729e371d916e7e75ea0d5f7
bensouici akram <bensouiciakram@gmail.com>
2025-10-13 14:23:26 +0100

    temporary accept all cors

--- price_alert/settings/prod.py
+++ price_alert/settings/prod.py
@@ -6,6 +6,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*'] # TODO : set it into the app subdomain 
 
+CORS_ALLOW_ALL_ORIGINS = True # TODO : remove later 
+
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('PRICE_ALERT_DATABASE_URL'))
 }
