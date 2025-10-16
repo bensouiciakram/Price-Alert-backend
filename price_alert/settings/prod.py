5d4c29b3ea0dd6df105037b4bd2d07ad7c92eb2e
bensouici akram <bensouiciakram@gmail.com>
2025-10-16 14:43:58 +0100

    adding CSRF_TRUSTED_ORIGINS

--- price_alert/settings/prod.py
+++ price_alert/settings/prod.py
@@ -7,6 +7,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['*'] # TODO : set it into the app subdomain 
 
 CORS_ALLOW_ALL_ORIGINS = True # TODO : remove later 
+CSRF_TRUSTED_ORIGINS = ['https://price-alert.akrambensouici.com']
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('PRICE_ALERT_DATABASE_URL'))
