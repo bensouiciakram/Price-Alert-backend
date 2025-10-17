9705b572aaacb0c53abea5c2b6997739d5b71a32
bensouici akram <bensouiciakram@gmail.com>
2025-10-17 11:41:58 +0100

    using only headless for camoufox

--- generic_scrapers/playwright.py
+++ generic_scrapers/playwright.py
@@ -8,7 +8,7 @@ class Extractor(BaseExtractor):
 
     def get_response(self) -> Selector :
         super().get_response()
-        with Camoufox(headless=False,slow_mo=1000) as browser:
+        with Camoufox(headless=True,slow_mo=1000) as browser:
             page = browser.new_page()
             page.goto(self.url)
             # page.wait_for_selector(self.title_xpath)
