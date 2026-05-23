3dd9f34c90b08f43dd99f46978edd72d6637408f
bensouici akram <bensouiciakram@gmail.com>
2026-05-23 10:43:07 +0100

    chore: add default dev secret key and ignore myenv directory
    - Provide a fallback SECRET_KEY for development when environment variable is not set
    - Add `myenv/` to .gitignore to exclude local virtual environment directory

--- price_alert/settings/comm.py
+++ price_alert/settings/comm.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('PRICE_ALERT_SECRET_KEY')
+SECRET_KEY = os.environ.get('PRICE_ALERT_SECRET_KEY', 'django-insecure-dev-key-change-in-production')
 
 INSTALLED_APPS = [
     "django.contrib.admin",
