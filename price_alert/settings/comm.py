1226ffaa920a572a747cde29c7878363fc9552d8
bensouici akram <bensouiciakram@gmail.com>
2026-05-30 10:39:09 +0100

    chore(settings): update static files config for Django 4.2+

--- price_alert/settings/comm.py
+++ price_alert/settings/comm.py
@@ -87,9 +87,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
